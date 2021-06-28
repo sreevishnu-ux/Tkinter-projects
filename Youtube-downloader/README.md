eb4a62ee5a56cae7c2c73099fd7b49afed14cd35
sreevishnu-ux <sreevishnuvinod@gmail.com>
2021-06-28 09:11:29 +0530

    added a new repo

---
+++ Youtube-downloader/README.md
@@ -0,0 +1,10 @@
+# Youtube-downloader
+This is a YouTube video downloader made using pytube and tkinter.
+By using this, we can defaultly download the video in the highest resolution.
+
+# Installation
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install pytyube.
+
+```bash
+pip install pytube
+```
---
+++ Youtube-downloader/source codde.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+from tkinter import ttk
+import os
+from pytube import YouTube
+from tkinter import messagebox as m_box
+import threading
+
+
+def Click():
+    got_link = link.get()
+    got_path = path.get()
+    try:
+        yt = YouTube(str(got_link))
+    except:
+        m_box.showerror("Error", "There is a Connection Problem !")
+    else:
+        vid = yt.streams.get_highest_resolution()
+        destination = str(got_path)
+        vid.download(destination)
+        os.startfile(got_path)
+        return m_box.showinfo('Successfully Downloaded.', f"Your YouTube Video is Downloaded Successfully at {got_path}/{yt.title}")
+
+
+threads = []
+
+
+def startThredProcess():
+    myNewThread = threading.Thread(target=Click)
+    threads.append(myNewThread)
+    myNewThread.start()
+
+win = tk.Tk()
+win.geometry("500x400")
+win.title("YouTube Video Downloader")
+win.minsize(500, 400)
+win.maxsize(500, 400)
+frame = ttk.LabelFrame(win)
+frame.grid(row=0, column=0, padx=70, pady=90)
+
+get_info = ttk.Label(frame, text="Enter YouTube Video Link : ")
+get_info.grid(row=0, column=0, sticky=tk.W)
+
+link = tk.StringVar()
+
+yt_link = ttk.Entry(frame, width=60, textvariable=link)
+yt_link.grid(row=1, columnspan=3, padx=0, pady=3)
+yt_link.focus()
+
+get_info = ttk.Label(frame, text="Enter Downloading Path : ")
+get_info.grid(row=3, column=0, sticky=tk.W)
+
+path = tk.StringVar()
+
+download_path = ttk.Entry(frame, width=60, textvariable=path)
+download_path.grid(row=4, columnspan=3, padx=0, pady=3)
+
+btn1 = ttk.Button(frame, text="Download Video",
+                  width=15, command=startThredProcess)
+btn1.grid(row=7, columnspan=3, padx=13, pady=7)
+
+win.mainloop()
